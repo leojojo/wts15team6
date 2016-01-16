a1f21873060932e463800dbb97612e9cf98fadd4
Leo Fujita <setsunal.fujita@gmail.com>
2016-01-16 16:20:00 +0900

    fixed indent

--- twitter_simple.py
+++ twitter_simple.py
@@ -56,53 +56,52 @@ os.system('chasen < output.txt > output.txt.chasen')
 names = []
 initial = ""
 
-#---インデントおかしい
 # 練習問題3(第4回課題)を参照
 for line in codecs.open('output.txt.chasen','r','utf-8'):
     line = line.rstrip('\r\n')
-  if line == "EOS":
+    if line == "EOS":
       print names
-    names = []
+      names = []
     else:
-        lis = line.split("\t")
-        if re.search(ur"人名",lis[3]):  #村井^Iムライ^I村井^I名詞-固有名詞-人名-姓^I^I
-            if re.search(ur"^[カキクケコ]", lis[1]):
-                initial = "K"
+      lis = line.split("\t")
+      if re.search(ur"人名",lis[3]):  #村井^Iムライ^I村井^I名詞-固有名詞-人名-姓^I^I
+        if re.search(ur"^[カキクケコ]", lis[1]):
+          initial = "K"
         elif re.search(ur"^[サシスセソ]", lis[1]):
           initial = "S"
-      elif re.search(ur"^[タツテト]", lis[1]):
+        elif re.search(ur"^[タツテト]", lis[1]):
           initial = "T"
-      elif re.search(ur"^[チ]", lis[1]):
+        elif re.search(ur"^[チ]", lis[1]):
           initial = "C"
-      elif re.search(ur"^[ナニヌネノ]", lis[1]):
+        elif re.search(ur"^[ナニヌネノ]", lis[1]):
           initial = "N"
-      elif re.search(ur"^[ハヒヘホ]", lis[1]):
+        elif re.search(ur"^[ハヒヘホ]", lis[1]):
           initial = "H"
-      elif re.search(ur"^[フ]", lis[1]):
+        elif re.search(ur"^[フ]", lis[1]):
           initial = "F"
-      elif re.search(ur"^[マミムメモ]", lis[1]):
+        elif re.search(ur"^[マミムメモ]", lis[1]):
           initial = "M"
-      elif re.search(ur"^[ヤユヨ]", lis[1]):
+        elif re.search(ur"^[ヤユヨ]", lis[1]):
           initial = "Y"
-      elif re.search(ur"^[ガギグゲゴ]", lis[1]):
+        elif re.search(ur"^[ガギグゲゴ]", lis[1]):
           initial = "G"
-      elif re.search(ur"^[ザジヂズヅゼゾ]", lis[1]):
+        elif re.search(ur"^[ザジヂズヅゼゾ]", lis[1]):
           initial = "Z"
-      elif re.search(ur"^[ダデド]", lis[1]):
+        elif re.search(ur"^[ダデド]", lis[1]):
           initial = "D"
-      elif re.search(ur"^[バビブベボ]", lis[1]):
+        elif re.search(ur"^[バビブベボ]", lis[1]):
           initial = "B"
-      elif re.search(ur"^[パピプペポ]", lis[1]):
+        elif re.search(ur"^[パピプペポ]", lis[1]):
           initial = "P"
-      elif re.search(ur"^[ア]", lis[1]):
+        elif re.search(ur"^[ア]", lis[1]):
           initial = "A"
-      elif re.search(ur"^[イ]", lis[1]):
+        elif re.search(ur"^[イ]", lis[1]):
           initial = "I"
-      elif re.search(ur"^[ウ]", lis[1]):
+        elif re.search(ur"^[ウ]", lis[1]):
           initial = "U"
-      elif re.search(ur"^[エ]", lis[1]):
+        elif re.search(ur"^[エ]", lis[1]):
           initial = "E"
-      elif re.search(ur"^[オ]", lis[1]):
+        elif re.search(ur"^[オ]", lis[1]):
           initial = "O"
 
 # 一旦false
